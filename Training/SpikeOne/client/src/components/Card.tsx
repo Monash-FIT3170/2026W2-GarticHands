782dee451d09598186c24ad9841b6eaffec10774
forrestdesu <fhua0018@student.monash.edu>
2026-04-04 19:27:01 +1100

    phase 2 card components

---
+++ Training/SpikeOne/client/src/components/Card.tsx
@@ -0,0 +1,13 @@
+import './Card.css'
+
+interface CardProps {
+  children: React.ReactNode
+}
+
+export default function Card({ children }: CardProps) {
+  return (
+    <div className="card">
+      {children}
+    </div>
+  )
+}
