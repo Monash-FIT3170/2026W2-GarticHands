2343903beb447593ebb7402ebec62e12d91b32ba
bexley07 <bexleyj07@gmail.com>
2026-04-08 01:55:12 +0800

    feat: initial widget layout with plain CSS

--- Training/SpikeOne/training/src/App.tsx
+++ Training/SpikeOne/training/src/App.tsx
@@ -1,120 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+    <div className='Phase1-Code'>
+      <h1>Phase 1, get something on screen</h1>
+
+      {/* Widget 1*/}
+      <section className='Counter-Widget'>
+        <h2>Interactive counter widget</h2>
+        <p>Count: {count}</p>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
+          <button 
+          className='Increase'
           onClick={() => setCount((count) => count + 1)}
+          >
+          Increase    
+          </button>
+          <button 
+          className='Decrease'
+          onClick={() => setCount((count) => count - 1)}
+          >
+          Decrease    
+          </button>
+        </div>
+      </section>
+
+      {/* Widget 2*/}
+      <section className='Text-Display'>
+        <h2>Display for text</h2>
+        <p className='text-data'>
+          The purpose of this widget is to display text.
+          Idk what else to put here, We can finally be Bees. 
+        </p>
+      </section>
+
+      {/* Widget 3*/}
+      <section className='Form-Input'>
+        <h2>Form for inputting information</h2>
+        <form 
+        onSubmit={(data) => {data.preventDefault();
+          alert("You really said that, okay then: " + text)
+        }}
         >
-          Count is {count}
-        </button>
+          <input type="text"
+          value={text}
+          onChange={(data) => setText(data.target.value)}
+          placeholder='Say something'
+          />
+          <button className='submit-Button' 
+          type='submit'>Show us what you said</button>
+        
+        </form>
       </section>
+    </div>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
   )
 }
 
