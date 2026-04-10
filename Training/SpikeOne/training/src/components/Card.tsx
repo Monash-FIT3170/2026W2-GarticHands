249a95ad64429aa9e6a483062e723d35aece708d
bexley07 <bexleyj07@gmail.com>
2026-04-10 15:18:55 +0800

    feat: migrate styling to Tailwind CSS

--- Training/SpikeOne/training/src/components/Card.tsx
+++ Training/SpikeOne/training/src/components/Card.tsx
@@ -4,7 +4,7 @@ interface CardProps {
 
 export default function Card({ children }: CardProps) {
   return (
-    <section className="Card">
+    <section className="w-[300px] m-5 p-5 border-2 border-grey-500 rounded-[10px] shadow-lg bg-white">
       <div> 
         {children}
       </div>
