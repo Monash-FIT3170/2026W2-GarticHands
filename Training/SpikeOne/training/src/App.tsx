21034c257185bd92e5c1286029762bf9ec2ac3d0
jayavi999 <jmeemaduma04@gmail.com>
2026-04-14 20:44:59 +1000

    feat: (phase-4) migrated styling to Tailwind CSS

--- Training/SpikeOne/training/src/App.tsx
+++ Training/SpikeOne/training/src/App.tsx
@@ -6,36 +6,43 @@ function App() {
   const [formValue, setFormValue] = useState("");
 
   return (
-    <div className="app-container">
+    <div className="text-center p-4 mx-auto max-w-2xl font-sans justify-items-center bg-emerald-200">
       {/* counter widget */}
       <Card>
-        <section className="counter-widget">
-          <h1>Counter</h1>
-          <button onClick={() => setCount((count) => count + 1)}>
+        <section className="border-2 border-blue-500 mx-auto rounded-lg p-4 justify-items-center flex-gap-4">
+          <h1 className="text-4xl font-bold text-blue-700">Counter</h1>
+          <button
+            className="bg-lime-500 cursor-pointer"
+            onClick={() => setCount((count) => count + 1)}
+          >
             Count is {count}
           </button>
         </section>
       </Card>
       {/* text display section */}
       <Card>
-        <section className="display-section">
-          <h1>Display Section</h1>
+        <section className="border-2 border-blue-500 mx-auto rounded-lg p-4 justify-items-center flex-gap-4">
+          <h1 className="text-4xl font-bold text-blue-700">Display Section</h1>
           <p className="display-text">This is a simple text display section.</p>
         </section>
       </Card>
       {/* form input widget */}
-      <Card>
-        <section className="input-widget">
-          <h1>Input Widget</h1>
-          <input
-            type="text"
-            value={formValue}
-            onChange={(e) => setFormValue(e.target.value)}
-            placeholder="Enter some text..."
-          />
-          <button>Submit</button>
-        </section>
-      </Card>
+      <section className="border-2 border-blue-500 mx-auto rounded-lg p-4 justify-items-center flex-gap-4">
+        <h1 className="text-4xl font-bold text-blue-700">Input Widget</h1>
+        <input
+          className="border-2 border-b-cyan-600"
+          type="text"
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="Enter some text..."
+        />
+        <button
+          className="bg-cyan-500 border-b-fuchsia-600 cursor-pointer"
+          onClick={() => console.log(formValue)}
+        >
+          Submit
+        </button>
+      </section>
     </div>
   );
 }
