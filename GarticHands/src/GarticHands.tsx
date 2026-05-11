710085f896dcdab31cb7a618b4b30f1791988f3b
bexley07 <bexleyj07@gmail.com>
2026-05-11 15:48:52 +1000

    Fixed package issues and got base working

--- GarticHands/src/GarticHands.tsx
+++ GarticHands/src/GarticHands.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link, Navigate, BrowserRouter } from 'react-router-dom'
 import Land from './Pages/landingPage.tsx'
 import Host from './Pages/hostingPage.tsx'
 import Join from './Pages/joiningPage.tsx'
+import Game from './Pages/gamePage.tsx'
 
 export default function GarticHands() {
 
@@ -9,6 +10,7 @@ export default function GarticHands() {
     <BrowserRouter>
       <div className='min-h-screen p-6'>
         <h1 className='text-3xl font-bold mb-6'>Gartic Hands</h1>
+        
         <nav className='flex gap-3 mb-8'>
           <Link to='/land-page' className='border px-4 py-2 rounded'>
             Join or Host a game
@@ -19,6 +21,9 @@ export default function GarticHands() {
           <Link to='/join' className='border px-4 py-2 rounded'>
             Joined Host
           </Link>
+          <Link to='/game' className='border px-4 py-2 rounded'>
+          GAMEPLAY
+          </Link>
         </nav>
 
         <Routes>
@@ -26,6 +31,7 @@ export default function GarticHands() {
           <Route path='/land-page' element={<Land />}/>
           <Route path='/host' element={<Host />}/>
           <Route path='/join' element={<Join />}/>
+          <Route path='/game' element={<Game />}/>
         </Routes>
       </div>
     </BrowserRouter>
