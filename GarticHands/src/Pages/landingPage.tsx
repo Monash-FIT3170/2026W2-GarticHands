57ce5717f93c89759200a86d0349390e7e1e6e41
jayavi999 <jmeemaduma04@gmail.com>
2026-05-18 16:31:09 +1000

    chore: removed guessRoom function

--- GarticHands/src/Pages/landingPage.tsx
+++ GarticHands/src/Pages/landingPage.tsx
@@ -11,10 +11,6 @@ export default function LandingPage() {
     navigate('/host')
   }
 
-  function guessRoom() {
-    navigate('/guess')
-  }
-
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-4">
       <h1 className='text-4xl font-bold'>Gartic Hands</h1>
