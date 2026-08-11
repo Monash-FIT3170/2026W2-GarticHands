c813e90a56f7f1c28177e97b7ddb56eef79f4732
bexley07 <bexleyj07@gmail.com>
2026-08-11 15:30:52 +1000

    <feature>(linting): Update ESLINT, incoporate prettier

--- GarticHands/client/eslint.config.js
+++ GarticHands/client/eslint.config.js
@@ -3,26 +3,64 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { 
+    ignores: [
+      'dist',
+      'node_modules',
+      'coverage'
+    ], 
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommendedTypeChecked,
+
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
+
     languageOptions: {
       ecmaVersion: 2023,
       globals: globals.browser,
+
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+
     rules: {
       ...reactHooks.configs.recommended.rules,
+
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+
+      // Generalised Code quality rules
+      'eqeqeq': ['error', 'always'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      'prefer-const': 'error',
+
+      // TypeScript
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
     },
   },
+
+  // NEEDS TO BE LAST - otherwise prettier won't format
+  eslintConfigPrettier,
 )
