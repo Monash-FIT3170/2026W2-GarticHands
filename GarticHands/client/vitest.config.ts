e82add003a8f5e4b929e3d17543cc48935505764
Ashutosh Shrivastav <ashr0018@student.monash.edu>
2026-08-19 05:37:01 +1000

    fix(config): Updated vitest.config.ts to actually include both the unit test and coverage test script when running since earlier, it was ignoring the unit tests

--- GarticHands/client/vitest.config.ts
+++ GarticHands/client/vitest.config.ts
@@ -8,17 +8,31 @@ const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     fs: {
       allow: [resolve(__dirname, '../')],
     },
   },
+
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: resolve(__dirname, '../component-tests/test-setup.ts'),
+
+    setupFiles: resolve(
+      __dirname,
+      '../component-tests/test-setup.ts'
+    ),
+
     include: [
-      resolve(__dirname, '../component-tests/**/*.{test,spec}.{ts,tsx}'),
+      resolve(
+        __dirname,
+        '../component-tests/**/*.{test,spec}.{ts,tsx}'
+      ),
+      resolve(
+        __dirname,
+        './unit-tests/**/*.{test,spec}.{ts,tsx}'
+      ),
     ],
   },
 })
